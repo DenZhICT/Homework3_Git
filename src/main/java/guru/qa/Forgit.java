092d9c4161eebba0c51e42692dd7e14e98931ddc
Журавлёв Денис <deniszhict@gmail.com>
2022-07-31 15:11:18 +0300

    add Forgit

---
+++ src/main/java/guru/qa/Forgit.java
@@ -0,0 +1,15 @@
+package guru.qa;
+
+import java.util.Scanner;
+
+public class Forgit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt(),b=sc.nextInt();
+        if (a==b){
+            System.out.println("You_are_cool!");
+        }else{
+            System.out.println("You_are_dead!");
+        }
+    }
+}
